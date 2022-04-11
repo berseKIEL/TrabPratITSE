93161e1f5ff0f4f12e471e407de9b18bdd751236
berseKIEL <103140962+berseKIEL@users.noreply.github.com>
2022-04-11 13:23:37 -0300

    Create Ejercicio4.py

---
+++ Programación/Ejercicios/ElemLeng/Ejercicio4.py
@@ -0,0 +1,11 @@
+'''
+Escribir un programa que pida al usuario dos números enteros y muestre por pantalla
+la <n> entre <m> da un cociente <c> y un resto <r> donde <n> y <m> son
+los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la
+división entera respectivamente.
+'''
+print('Ejercicio 4')
+n= int(input('Ingrese dividiendo'))
+m = int(input('Ingrese divisor'))
+print('el cociente es: ', n/m)
+print('el resto es:', n%m)
