74df7b229a2fa94845a2a806a8f8130d6824603d
berseKIEL <103140962+berseKIEL@users.noreply.github.com>
2022-04-11 13:23:23 -0300

    Create Ejercicio3.py

---
+++ Programación/Ejercicios/ElemLeng/Ejercicio3.py
@@ -0,0 +1,9 @@
+'''
+Escribir un programa que pregunte al usuario por la cantidad de un producto solicitado
+y el precio por unidad. Después debe mostrar por pantalla el costo total.
+'''
+print('Ejercicio 3')
+producto=input('Indique el producto a comprar:\n')
+cantidad=input(f'Indique la cantidad del producto {producto} a comprar:\n')
+precio = 99
+print(f'Usted compro {cantidad} de {producto} a {precio} c/u\n')
