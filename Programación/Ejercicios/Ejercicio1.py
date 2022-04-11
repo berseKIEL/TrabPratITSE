060e79117ec6e82cfdca38db11c31daf128e93ce
berseKIEL <103140962+berseKIEL@users.noreply.github.com>
2022-04-11 13:24:40 -0300

    Delete Ejercicio1.py

--- Programación/Ejercicios/Ejercicio1.py
+++
@@ -1,8 +0,0 @@
-'''
-Escribir un programa que solicite el nombre del usuario en la consola y mostrar por
-pantalla la cadena ¡Bienvenido <name>!, donde <name> es el nombre que el
-usuario haya introducido.
-'''
-print('Ejercicio 1')
-usuario = input("ingrese un nombre de usuario: ")
-print(f"Bienvenido, {usuario}\n")
