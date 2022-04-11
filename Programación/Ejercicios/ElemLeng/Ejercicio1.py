73518c32f39ec5a06ee1e51fb8d984bd3615e94c
berseKIEL <103140962+berseKIEL@users.noreply.github.com>
2022-04-11 13:22:59 -0300

    Create Ejercicio1.py

---
+++ Programación/Ejercicios/ElemLeng/Ejercicio1.py
@@ -0,0 +1,8 @@
+'''
+Escribir un programa que solicite el nombre del usuario en la consola y mostrar por
+pantalla la cadena ¡Bienvenido <name>!, donde <name> es el nombre que el
+usuario haya introducido.
+'''
+print('Ejercicio 1')
+usuario = input("ingrese un nombre de usuario: ")
+print(f"Bienvenido, {usuario}\n")
